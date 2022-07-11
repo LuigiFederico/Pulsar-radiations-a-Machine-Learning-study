37b26c441392a3a3001a979317461212d0a65931
Alex Carluccio <alexcarluccio@live.it>
2022-07-11 15:41:12 +0200

    Little Fix

--- Project/main.py
+++ Project/main.py
@@ -56,23 +56,21 @@ def MVG_models(subsets, splits, prior, K):
         print(title)
         for k in MVG.MVG_models.keys():
             minDCF = numpy.round( MVG.kfold_MVG(k_subsets, K, prior, MVG.MVG_models[k]), 3)
-            for p in prior:
-                print("- %s, prior = %.2f, minDCF = %.3f" % (k, p, minDCF))
-        print()
+            for i,p in enumerate(prior):
+                print("- %s, prior = %.2f, minDCF = %.3f" % (k, p, minDCF[i]))
         
 
-    def single_split_MVG_compute(split, prior, title=''):
-        
-        print(title)
-        for k in MVG.MVG_models.keys():
-            minDCF = numpy.round( MVG.single_split_MVG(split, prior, MVG.MVG_models[k]), 3)
-            for p in prior:
-                print("- %s, prior = %.2f, minDCF = %.3f" % (k, p, minDCF))
-        print()
+    #def single_split_MVG_compute(split, prior, title=''):
+    #    
+    #    print(title)
+    #    for k in MVG.MVG_models.keys():
+    #        minDCF = numpy.round( MVG.single_split_MVG(split, prior, MVG.MVG_models[k]), 3)
+    #        for i,p in enumerate(prior):
+    #            print("- %s, prior = %.2f, minDCF = %.3f" % (k, p, minDCF[i]))
     
-    TrainMod = ['Training dataset',
-                'Training dataset + PCA m = 7',
-                'Training dataset + PCA m = 6',
+    TrainMod = ['Raw dataset',
+                'Raw dataset + PCA m = 7',
+                'Raw dataset + PCA m = 6',
                 'Gaussianized dataset',
                 'Gaussianized dataset + PCA m = 7',
                 'Gaussianized dataset + PCA m = 6']
@@ -83,9 +81,9 @@ def MVG_models(subsets, splits, prior, K):
     for (i, TrainLabel) in enumerate(TrainMod):
         kfold_MVG_compute(subsets[i], K, prior, TrainLabel)
      
-    print('----- SINGLE SPLIT -----\n')
-    for(i, TrainLabel) in enumerate(TrainMod):
-        single_split_MVG_compute(splits[i], prior, TrainLabel)
+    #print('----- SINGLE SPLIT -----\n')
+    #for(i, TrainLabel) in enumerate(TrainMod):
+    #    single_split_MVG_compute(splits[i], prior, TrainLabel)
 
     
 def LR_models(subsets, splits, prior, K, lambdas, quadratic=False, pi_t=0.5):
@@ -95,10 +93,10 @@ def LR_models(subsets, splits, prior, K, lambdas, quadratic=False, pi_t=0.5):
         
         for pi_T in pi_t:
             for p in prior :
-                minDCF_values,_ = f(k_subsets, K, lambdas, p, pi_T)
+                minDCF_values = f(k_subsets, K, lambdas, p, pi_T)
                 minDCF_LR.append(minDCF_values)
             #plt.plot_DCF(lambdas, minDCF_LR, "λ")
-            print (numpy.around(minDCF_LR, 3)) # rounded
+            #print (numpy.around(minDCF_LR, 3)) # rounded
         
         return minDCF_LR
     
@@ -107,17 +105,17 @@ def LR_models(subsets, splits, prior, K, lambdas, quadratic=False, pi_t=0.5):
         minDCF_LR = [] 
         for pi_T in pi_t:
             for p in prior :
-                minDCF_values,_  = f( split , lambdas, p, pi_T)
+                minDCF_values  = f( split , lambdas, p, pi_T)
                 minDCF_LR.append(minDCF_values)
             #plt.plot_DCF(lambdas, minDCF_LR, "λ")
-            print (numpy.around(minDCF_LR, 3)) # rounded
+            #print (numpy.around(minDCF_LR, 3)) # rounded
         
         return minDCF_LR    
     
     
-    TrainMod=["Training dataset",
-              "Training dataset with PCA m = 7",
-              "Training dataset with PCA m = 6",
+    TrainMod=["Raw dataset",
+              "Raw dataset with PCA m = 7",
+              "Raw dataset with PCA m = 6",
               "Gaussianized dataset","Gaussianized with PCA m = 7",
               "Gaussianized with PCA m = 6"]    
         
@@ -129,50 +127,48 @@ def LR_models(subsets, splits, prior, K, lambdas, quadratic=False, pi_t=0.5):
             print('\n',TrainLabel)
             kfold_LR_compute(subsets[i], K, lambdas, prior, pi_t, LR.kfold_LogReg)
         
-        print('------- SINGLE SPLIT -------')
-        for (i,TrainLabel) in enumerate(TrainMod):
-            print('\n',TrainLabel)
-            single_split_LR_compute(splits[i], lambdas, prior, pi_t, LR.single_split_LogReg)
+        #print('------- SINGLE SPLIT -------')
+        #for (i,TrainLabel) in enumerate(TrainMod):
+        #    print('\n',TrainLabel)
+        #    single_split_LR_compute(splits[i], lambdas, prior, pi_t, LR.single_split_LogReg)
             
     else:
         print('####  Quadratic LR  ####\n')
         
-        print('------- K FOLD -------')
+        print('-------  %d-FOLD  -------\n' % K)
         k_subsets, k_subsets_PCA7, k_subsets_PCA6, k_gauss_subsets, k_gauss_PCA7_subs, k_gauss_PCA6_subs = subsets 
         
-        print('\nTraining dataset')
+        print('\nRaw dataset')
         kfold_LR_compute(k_subsets, K, lambdas, prior, pi_t, LR.kfold_QuadLogReg)
         
         print('\nGaussianized dataset')
         kfold_LR_compute(k_gauss_subsets, K, lambdas, prior, pi_t, LR.kfold_QuadLogReg)
         
-        print('------- SINGLE SPLIT -------')
-        train_split, train_PCA7_split, train_PCA6_split, gauss_split, gauss_PCA7_split, gauss_PCA6_split = splits
+        #print('------- SINGLE SPLIT -------')
+        #train_split, train_PCA7_split, train_PCA6_split, gauss_split, gauss_PCA7_split, gauss_PCA6_split = splits
         
-        print('\nTraining dataset')
-        single_split_LR_compute(train_split, lambdas, prior, pi_t, LR.single_plit_QuadLogReg)
+        #print('\nRaw dataset')
+        #single_split_LR_compute(train_split, lambdas, prior, pi_t, LR.single_split_QuadLogReg)
         
-        print('\nGaussianized dataset')
-        single_split_LR_compute(gauss_split, lambdas, prior, pi_t, LR.single_split_QuadLogReg)
+        #print('\nGaussianized dataset')
+        #single_split_LR_compute(gauss_split, lambdas, prior, pi_t, LR.single_split_QuadLogReg)
     
         
 def SVM_models(subsets, splits, prior, K, Cs, pi_t=0.5, mode='linear'):
     
-    def single_split_SVM(split, prior, Cs, pi_t, f, mode):
-        minDCF_SVM = []
-        
-        if mode == 'balanced-linear':
-            for pi_T in pi_t:
-                for p in prior:
-                    minDCF_values, LabelPredicetd = f(split, Cs, pi_T, p, mode)
-            print (numpy.around(minDCF_SVM, 3)) # rounded
-        else:
-            for p in prior :
-                minDCF_values, LabelPredicetd = f(split, Cs, pi_t, p, mode)
-                minDCF_SVM.append(minDCF_values)
-            print (numpy.around(minDCF_SVM, 3)) # rounded
-        
-        return minDCF_SVM
+    #def single_split_SVM(split, prior, Cs, pi_t, f, mode):
+    #    minDCF_SVM = []
+    #    
+    #    if mode == 'balanced-linear':
+    #        for pi_T in pi_t:
+    #            for p in prior:
+    #                minDCF_values, LabelPredicetd = f(split, Cs, pi_T, p, mode)
+    #    else:
+    #        for p in prior :
+    #            minDCF_values, LabelPredicetd = f(split, Cs, pi_t, p, mode)
+    #            minDCF_SVM.append(minDCF_values)
+    #    
+    #    return minDCF_SVM
     
     
     def kfold_SVM(k_subsets, prior, K, Cs, pi_t, f, mode='linear', title=''):
@@ -181,14 +177,14 @@ def SVM_models(subsets, splits, prior, K, Cs, pi_t=0.5, mode='linear'):
         if mode == 'balanced-linear':
             for pi_T in pi_t:
                 for p in prior:
-                    minDCF_values, LabelPredicetd = f(k_subsets, Cs, pi_T, p, K, mode)
+                    minDCF_values = f(k_subsets, Cs, pi_T, p, K, mode)
                     minDCF_SVM.append(minDCF_values)
             print (numpy.around(minDCF_SVM, 3)) # rounded
         
         elif mode=='poly': # prior=0.5
             c_vec = [0, 1, 10, 30]
             for c in c_vec:
-                minDCF_values, LabelPredicted = f(k_subsets, Cs, c=c, mode=mode)
+                minDCF_values = f(k_subsets, Cs, c=c, mode=mode)
                 minDCF_SVM.append(minDCF_values)
             plt.plot_DCF(Cs, minDCF_SVM, "C", title)
             print (numpy.around(minDCF_SVM), 3)
@@ -196,7 +192,7 @@ def SVM_models(subsets, splits, prior, K, Cs, pi_t=0.5, mode='linear'):
         elif mode=='RBF':
             gammas=numpy.logspace(-3,-1,3) # prior=0.5
             for gamma in gammas:
-                minDCF_values, LabelPredicetd = f(k_subsets, Cs, pi_t, 0.5, K, mode, gamma)
+                minDCF_values = f(k_subsets, Cs, pi_t, 0.5, K, mode, gamma)
                 minDCF_SVM.append(minDCF_values)
             print (numpy.around(minDCF_SVM, 3)) # rounded
             plt.plot_DCF(Cs, minDCF_SVM, "C")
@@ -207,7 +203,7 @@ def SVM_models(subsets, splits, prior, K, Cs, pi_t=0.5, mode='linear'):
                     
         else:            
             for p in prior:
-                minDCF_values, LabelPredicetd = f(k_subsets, Cs, pi_t, p, K, mode)
+                minDCF_values = f(k_subsets, Cs, pi_t, p, K, mode)
                 minDCF_SVM.append(minDCF_values)
             print (numpy.around(minDCF_SVM, 3)) # rounded
         
@@ -216,25 +212,25 @@ def SVM_models(subsets, splits, prior, K, Cs, pi_t=0.5, mode='linear'):
     
     print('########  %s SVM  ########\n' % mode)
     
-    # print('------- SINGLE SPLIT -------')
-    # train_split, _, _, gauss_split, _, _ = splits
+    #print('------- SINGLE SPLIT -------')
+    #train_split, _, _, gauss_split, _, _ = splits
     
-    # print('\nTraining dataset')
-    # single_split_SVM(train_split, prior, Cs, pi_t, SVM.single_split_SVM, mode)
+    #print('\nRaw dataset')
+    #single_split_SVM(train_split, prior, Cs, pi_t, SVM.single_split_SVM, mode)
     
-    # print('\nGaussianized dataset')
-    # single_split_SVM(gauss_split, prior, Cs, pi_t, SVM.single_split_SVM, mode)
+    #print('\nGaussianized dataset')
+    #single_split_SVM(gauss_split, prior, Cs, pi_t, SVM.single_split_SVM, mode)
     
     
     print('-------  %d-FOLD  -------\n' % K)
     
     k_subsets, k_subsets_PCA7, k_subsets_PCA6, k_gauss_subsets, k_gauss_PCA7_subs, k_gauss_PCA6_subs = subsets    
     
-    print('\nTraining dataset')
+    print('\nRaw dataset')
     kfold_SVM(k_subsets, prior, K, Cs, pi_t, SVM.kfold_SVM, mode, title="SVM_Raw_prior5")
     
-    # print('\nGaussianized dataset')
-    # kfold_SVM(k_guass, prior, K, Cs, pi_t, SVM.kfold_SVM, mode)
+    #print('\nGaussianized dataset')
+    #kfold_SVM(k_guass, prior, K, Cs, pi_t, SVM.kfold_SVM, mode)
     
     print('\nGaussianized dataset + PCA m=6')
     kfold_SVM(k_gauss_PCA6_subs, prior, K, Cs, pi_t, SVM.kfold_SVM, mode, title="SVM_GaussPCA6_prior5")
@@ -268,19 +264,19 @@ def GMM_models(subsets, splits, prior, K, alpha, nComponents, mode='full', psi=0
 
     print('########  GMM - %s-cov  ########\n' % mode)
      
-    # print('------- SINGLE SPLIT -------')
-    # train_split, _, _, gauss_split, _, _ = splits
+    print('------- SINGLE SPLIT -------')
+    train_split, _, _, gauss_split, _, _ = splits
     
-    # print('\nTraining dataset')    
-    # single_split_GMM(train_split, prior, alpha, nComponents, mode, psi)
+    print('\nRaw dataset')    
+    single_split_GMM(train_split, prior, alpha, nComponents, mode, psi)
     
-    # print('\nGaussianized dataset')    
-    # single_split_GMM(gauss_split, prior, alpha, nComponents, mode, psi)
+    print('\nGaussianized dataset')    
+    single_split_GMM(gauss_split, prior, alpha, nComponents, mode, psi)
     
     print('-------  %d-FOLD  -------\n' % K)
     k_subset, _, _, k_guass, _, _ = subsets    
     
-    print('\nTraining dataset')    
+    print('\nRaw dataset')    
     kfold_GMM(k_subset, K, prior, alpha, 
               nComponents, mode, psi, 'GMM-'+mode+' Train')
     
@@ -529,18 +525,18 @@ if __name__ == '__main__':
     #------------------------------#
     #  Model training with K-FOLD  #
     #------------------------------#
-    K = 5  # k-fold 
+    K = 2  # k-fold 
     prior = [0.5, 0.9, 0.1]
     pi_t = [0.5, 0.9, 0.1]
     
-    lambdas=numpy.logspace(-5,-5, 1)         #For Normal Use
+    lambdas=numpy.logspace(-5,-5, 1)        #For Normal Use
     #lambdas=numpy.logspace(-5, 2, num=30)  #For Graphichs Use
     
-    Cs = numpy.logspace(-1, -1, num=1)  #For Normal Use
-    #Cs = numpy.logspace(-4, -1, num=10) #For Graphichs Use
+    Cs = numpy.logspace(-1, -1, num=1)      #For Normal Use
+    #Cs = numpy.logspace(-4, -1, num=10)    #For Graphichs Use
     
-    nComponents = [8] #For Normal Use
-    nComponents = [2, 4, 8, 16, 32] #For Normal Use
+    nComponents = [8]                       #For Normal Use
+    #nComponents = [2, 4, 8, 16, 32]        #For Graphichs Use
     
     # K-fold
     subsets = prep.kfold_computeAll(D_Train, D_Gaussianization, L_Train, K)
@@ -551,24 +547,27 @@ if __name__ == '__main__':
     # MVG 
     #MVG_models(subsets, splits, prior, K)
     
+    
     # LR
     #LR_models(subsets, splits, prior, K , lambdas, pi_t=[0.5])
     
+    
     # QLR
     #LR_models(subsets, splits, prior, K, lambdas, quadratic=True, pi_t=[0.5])
+    
     
     # SVM
     #SVM_models(subsets, splits, prior, K, Cs, pi_t, 'linear')
     #SVM_models(subsets, splits, prior, K, Cs, pi_t, 'balanced-linear')
     #SVM_models(subsets, splits, prior, K, Cs, pi_t, 'poly')
     #SVM_models(subsets, splits, prior, K, Cs, pi_t, 'RBF')
-
+    
     
     # GMM    
-    #GMM_models(subsets, splits, prior, K, alpha=0.1, nComponents=nComponents, mode='full', psi=0.01)
-    #GMM_models(subsets, splits, prior, K, alpha=0.1, nComponents=nComponents, mode='diag', psi=0.01)
-    #GMM_models(subsets, splits, prior, K, alpha=0.1, nComponents=nComponents, mode='tied-full', psi=0.01)
-    #GMM_models(subsets, splits, prior, K, alpha=0.1, nComponents=nComponents, mode='tied-diag', psi=0.01)
+    GMM_models(subsets, splits, prior, K, alpha=0.1, nComponents=nComponents, mode='full', psi=0.01)
+    GMM_models(subsets, splits, prior, K, alpha=0.1, nComponents=nComponents, mode='diag', psi=0.01)
+    GMM_models(subsets, splits, prior, K, alpha=0.1, nComponents=nComponents, mode='tied-full', psi=0.01)
+    GMM_models(subsets, splits, prior, K, alpha=0.1, nComponents=nComponents, mode='tied-diag', psi=0.01)
     
         
     #----------------------------------#  
@@ -675,9 +674,6 @@ if __name__ == '__main__':
     GMM.GMM_EVALUATION(Split_GaussPCA_Test, prior, mode="tied-full", alpha=0.1, Component=8, psi=0.01, lambd_calib=1e-4)
     GMM.GMM_EVALUATION(Split_GaussPCA_Test, prior, mode="tied-diag", alpha=0.1, Component=16, psi=0.01, lambd_calib=1e-4)
 
-
-    # # We need to replicate the analisys done before
-    # ROC(subsets_Test, K, [0.5], isTrainSet=False)
     
     
     
